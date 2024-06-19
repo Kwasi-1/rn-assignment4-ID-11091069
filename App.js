e933947b58b9af4ac8530f7513a934cfd9db2fbe
kwasi <affulnana04@gmail.com>
2024-06-19 20:15:17 +0000

    the start of a beautiful project

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import 'react-native-gesture-handler';
+import Home from './Home';
+import Login from './Login';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='login' screenOptions={{headerShown: false}}>
+        <Stack.Screen name= 'login' component={Login}/>
+        <Stack.Screen name= 'Home' component = {Home}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
